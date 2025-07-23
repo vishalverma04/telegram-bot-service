5d264ccda1aa0a83dc5a64be33ed745a0f85e823
Samant K Mishra <mishrasamantkumar1@gmail.com>
2025-07-23 22:41:18 +0530

    Initialize backend

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env'});
+
+import {app} from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
